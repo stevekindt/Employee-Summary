713459901341d4ea3bd606bebfa1e0ba4f150223
Steve Kindt <stevemkindt@gmail.com>
2020-02-29 08:39:10 -0600

    new rendered html

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./lib/htmlRenderer");
 ​
-​const teamMembers = [];
+​let devTeam = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​inquirer
