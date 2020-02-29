d4a363ed8569cdcbb8557cdc46697f5a003d5c65
Steve Kindt <stevemkindt@gmail.com>
2020-02-29 17:34:50 -0600

    employees js

--- app.js
+++ app.js
@@ -1,100 +1,148 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const generateHtml = require("./output/teamHTML");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​let devTeam = [];
 
-const employeeTypeInquiry = [
-    {
-      type: "list",
-      message: "Select Employee's Job Type",
-      name: "role",
-      choices: [
-        "Engineer",
-        new inquirer.Separator(),
-        "Intern",
-        new inquirer.Separator(),
-        "Done - assemble development team"
-      ]
-    }
-  ];
+//Initial Prompt
+function menu() {
+    inquirer.prompt({
+       type: "list",
+       name: "choices",
+       message: "What would you like to do?",
+       choices: ["Add a manager", "Add an intern", "Add an engineer", "Exit and write HTML"]
+    }).then(answers => {
+        if (answers.choices === "Add a manager") {
+            promptManager()
+        };
+        if (answers.choices === "Add an intern") {
+            promptIntern()
+        };
+        if (answers.choices === "Add an engineer")  {
+            promptEngineer() 
+        };
+        if (answers.choices === "Exit and write HTML")  {
+            exit()
+        };
+    });
+};
 
-const managerInquiries = [
-    {
-      type: "input",
-      message: "What is the manager's name?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "What is the manager's email?",
-      name: "email"
-    },
-    {
-      type: "number",
-      message: "What is the manager's employee ID number?",
-      name: "id"
-    },
-    {
-      type: "number",
-      message: "What is the manager's office number?",
-      name: "officenumber"
-    }
-];
+//Manager Questions
+promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the manager's name."
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your ID."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email."
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter your office Number."
+        },
+    ]).then(manager => {
+      let managerHtml = `<div class="card"><h3><i class="fas fa-teeth-open"></i>&ensp;Manager</h3><h4>${manager.name}</h4><p>${manager.id}</p><p>${manager.email}</p><p>${manager.officeNumber}</p></div>`;
+            fs.appendFile("index.html", managerHtml, (err) => {
+                if (err) throw err;
+                console.log("manager added");
+                menu();
+        });
+    });          
+};
 
-const engineerInquiries = [
-    {
-        type: "input",
-        message: "What is the engineer's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the engineer's email?",
-        name: "email"
-    },
-    {
-        type: "number",
-        message: "What is the engineer's employee ID number?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What is this engineer's GitHub username?",
-        name: "github"
-    }
-];
+//Intern Questions
+promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the intern's name."
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the intern's ID."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the intern's email."
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter the intern's school."
+        },
+    ]).then(intern => {
+        let internHtml = `<div class="card"><h3><i class="fas fa-graduation-cap"></i>&ensp;Intern</h3><h4>${intern.name}</h4><p>${intern.id}</p><p>${intern.email}</p><p>${intern.school}</p></div>`;
+        fs.appendFile("index.html", internHtml, 'utf8', (err) => {
+            if (err) throw err;
+            console.log("intern added");
+            menu();
+        });
+    });        
+};
 
-const internInquiries = [
-    {
-        type: "input",
-        message: "What is the intern's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the intern's email?",
-        name: "email"
-    },
-    {
-        type: "number",
-        message: "What is the intern's employee ID number?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What school did this intern attend?",
-        name: "school"
-    }
-];
+//Engineer Questions
+promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the engineer's name."
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the engineer's ID."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the engineer's email."
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter the engineer's GitHub profile."
+        },
+    ]).then(engineer => {
+        let engineerHtml = `<div class="card"><h3><i class="fas fa-pencil-alt"></i>&ensp;Engineer</h3><h4>${engineer.name}</h4><p>${engineer.id}</p><p>${engineer.email}</p><p>${engineer.github}</p></div>`;
+        
+            fs.appendFile("index.html", engineerHtml, (err) => {
+                if (err) throw err;
+                console.log("engineer added");
+                menu();
+            });
+        });         
+};
+
+
+//Exit and write HTML
+exit = () =>    {
+    let footer = `</div> </html>`;
+    fs.appendFile("index.html", footer, (err) => {
+        if (err) throw err;
+                console.log("html ended");
+    })
+}
+
+menu();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
