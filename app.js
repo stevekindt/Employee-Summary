1274a4cad5a60120daff1826307158d01fd2ea53
Steve Kindt <stevemkindt@gmail.com>
2020-02-29 22:48:40 -0600

    all test pass

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 ​
-// const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.resolve(__dirname, "output", "team.html");
 ​
 const render = require("./lib/htmlRenderer");
